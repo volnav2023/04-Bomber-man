194791a0df0391388d9fc25295632e0ee53e6954
volnav2023 <github@flechet.com>
2020-06-30 11:40:48 +0200

    detect updownleftright and distinct action on each

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,27 @@
-// function disp(str){
-// //alert(str);
-// document.getElementById('my_msg').innerHTML=str;
-// }
-
-// document.onkeydown = function() {
-//     switch (window.event.keyCode) {
-//         case 37:
-//          disp('Left key is pressed') // execute a function by passing parameter 
-//          break;
-//         case 38:
-//          disp('Up key is pressed') 
-//          break;
-//         case 39:
-// 		disp('Right key is pressed') 
-//          break;
-//         case 40:
-// 		disp('Down key is pressed')  
-//         break;
-//     }
-// };
 
 let player = document.querySelector("#player");
 
 console.log(player);
 
 function removewhite() {
-     player.classList.remove("whiteman");
+    player.classList.remove("whiteman");
 }
-// function setwhite() {
-//     player.classList.add("whiteman")
-// }
 
-player.addEventListener("click", removewhite);
+document.addEventListener("keydown", removewhite);
+
+document.onkeydown = function () {
+    switch (window.event.keyCode) {
+        case 37:
+            console.log('Left key is pressed');
+            break;
+        case 38:
+            console.log('Up key is pressed');
+            break;
+        case 39:
+            console.log('Right key is pressed');
+            break;
+        case 40:
+            console.log('Down key is pressed');
+            break;
+    }
+};
