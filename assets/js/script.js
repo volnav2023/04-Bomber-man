c4f68a057163c56348eb8c66390401d8330270fd
volnav2023 <github@flechet.com>
2020-06-30 15:01:06 +0200

    limites implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
-var playerx = 10;
-var playery = 10;
-const maxx = 21;
-const maxy = 21;
+// var playerh = document.getElementById('playerman').style.height;
+// var playerw = document.getElementById('playerman').style.width;
+var playerh = 30;
+var playerw = 30;
+var boardh = 600;
+var boardw = 600;
+var playerx = boardh / playerh / 2;
+var playery = boardw / playerw / 2;
+const maxx = boardh / playerh;
+const maxy = boardw / playerw;
 const minx = -1;
 const miny = -1;
 // var playerm = $("#playerman");
+let playerid = document.querySelector("#playerman");
 
-
-
-let player = document.querySelector("#playerm");
-
-console.log(player);
 
 function removewhite() {
-    player.classList.remove("whiteman");
+    playerid.classList.remove("whiteman");
 }
 
 function moveplayerleft() {
-    player.classList.remove("whiteman");
+    playerid.classList.remove("whiteman");
 }
 
 // document.addEventListener("keydown", removewhite);
@@ -27,46 +29,45 @@ document.onkeydown = function () {
         case 37:
             console.log('Left key is pressed');
             playerx--;
-            if (playerx == minx) {
+            if (playerx <= minx) {
                 playerx = minx + 1;
-                document.getElementById('posX').innerHTML = playerx;
-                document.getElementById('posY').innerHTML = playery;
             }
             break;
         case 38:
             console.log('Up key is pressed');
             playery--;
-            if (playery == miny) {
+            if (playery <= miny) {
                 playery = miny + 1;
-                document.getElementById('posX').innerHTML = playerx;
-                document.getElementById('posY').innerHTML = playery;
             } break;
         case 39:
             console.log('Right key is pressed');
             playerx++;
-            if (playerx == maxx) {
+            if (playerx >= maxx) {
                 playerx = maxx - 1;
-                document.getElementById('posX').innerHTML = playerx;
-                document.getElementById('posY').innerHTML = playery;
             }
             break;
         case 40:
             console.log('Down key is pressed');
             playery++;
-            if (playery == maxy) {
+            if (playery >= maxy) {
                 playery = maxy - 1;
-                document.getElementById('posX').innerHTML = playerx;
-                document.getElementById('posY').innerHTML = playery;
             }
             break;
     }
-    
-    
+
+
     // player.style.left = 300+"px";
 
     // document.getElementById('#playerman').left = (playerx);
 
-    document.getElementById('playerman').style.left = playerx+"px";
+    document.getElementById('playerman').style.left = playerx * playerw + "px";
+    document.getElementById('playerman').style.top = playery * playerh + 12.5+"px";
+    document.getElementById('posX').innerHTML = playerx;
+    document.getElementById('posY').innerHTML = playery;
+
+    console.log(playerid);
+    console.log(playerh);
+
     // document.getElementById('texto').innerText = "Textb";
 
 };
