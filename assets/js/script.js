4e179e0c3bb1cad3eaa842a2b3c1ee2fc0ec9a20
volnav2023 <github@flechet.com>
2020-06-30 12:03:14 +0200

    update player position incl board limits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+var playerx = 10;
+var playery = 10;
+const maxx = 21;
+const maxy = 21;
+const minx = -1;
+const miny = -1;
+
 
 let player = document.querySelector("#player");
 
@@ -7,21 +14,43 @@ function removewhite() {
     player.classList.remove("whiteman");
 }
 
-document.addEventListener("keydown", removewhite);
+function moveplayerleft() {
+    player.classList.remove("whiteman");
+}
+
+// document.addEventListener("keydown", removewhite);
 
 document.onkeydown = function () {
     switch (window.event.keyCode) {
         case 37:
             console.log('Left key is pressed');
+            playerx--;
+            if (playerx == minx) {
+                playerx = minx + 1;
+            }
             break;
         case 38:
             console.log('Up key is pressed');
-            break;
+            playery--;
+            if (playery == miny) {
+                playery = miny + 1;
+            } break;
         case 39:
             console.log('Right key is pressed');
+            playerx++;
+            if (playerx == maxx) {
+                playerx = maxx - 1;
+            }
             break;
         case 40:
             console.log('Down key is pressed');
+            playery++;
+            if (playery == maxy) {
+                playery = maxy - 1;
+            }
             break;
     }
+    console.log(playerx);
+    console.log(playery);
+
 };
