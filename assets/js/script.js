aa49f6f849f6d1d663cb2feeeb4135ad77902aef
volnav2023 <github@flechet.com>
2020-07-01 13:54:35 +0200

    ennemies moving on key press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
 // var playerh = document.getElementById('playerman').style.height;
 // var playerw = document.getElementById('playerman').style.width;
-var playerh = 30;
-var playerw = 30;
+// var playerw = document.getElementById('playerman').style.getPropertyValue("width");
+//     console.log(playerw);
+
+
 var boardh = 600;
 var boardw = 600;
+var playerh = 30;
+var playerw = 30;
 var playerx = boardh / playerh / 2;
 var playery = boardw / playerw / 2;
+var ennemyh = 30;
+var ennemyw = 30;
+var ennemy1x = boardh / ennemyh / 2;
+var ennemy1y = boardw / ennemyw / 2;
+var ennemy2x = boardh / ennemyh / 2;
+var ennemy2y = boardw / ennemyw / 2;
+var ennemy3x = boardh / ennemyh / 2;
+var ennemy3y = boardw / ennemyw / 2;
 const maxx = boardh / playerh;
 const maxy = boardw / playerw;
 const minx = -1;
 const miny = -1;
-// var playerm = $("#playerman");
-let playerid = document.querySelector("#playerman");
 
+let playerid = document.querySelector("#playerman");
 
 function removewhite() {
     playerid.classList.remove("whiteman");
@@ -38,7 +49,8 @@ document.onkeydown = function () {
             playery--;
             if (playery <= miny) {
                 playery = miny + 1;
-            } break;
+            }
+            break;
         case 39:
             console.log('Right key is pressed');
             playerx++;
@@ -55,19 +67,49 @@ document.onkeydown = function () {
             break;
     }
 
+    ennemy1x = ennemy1x + Math.round(Math.random()) - 0.5;
+    ennemy1y = ennemy1y + Math.round(Math.random()) - 0.5;
+    ennemy2x = ennemy2x + Math.round(Math.random()) - 0.5;
+    ennemy2y = ennemy2y + Math.round(Math.random()) - 0.5;
+    ennemy3x = ennemy3x + Math.round(Math.random()) - 0.5;
+    ennemy3y = ennemy3y + Math.round(Math.random()) - 0.5;
 
-    // player.style.left = 300+"px";
-
-    // document.getElementById('#playerman').left = (playerx);
+    if (ennemy1x <= minx) { ennemy1x = minx + 1; }
+    if (ennemy1y <= miny) { ennemy1y = miny + 1; }
+    if (ennemy1x >= maxx) { ennemy1x = maxx - 1; }
+    if (ennemy1y >= maxy) { ennemy1y = maxy - 1; }
+    
+    if (ennemy2x <= minx) { ennemy2x = minx + 1; }
+    if (ennemy2y <= miny) { ennemy2y = miny + 1; }
+    if (ennemy2x >= maxx) { ennemy2x = maxx - 1; }
+    if (ennemy2y >= maxy) { ennemy2y = maxy - 1; }
+    
+    if (ennemy3x <= minx) { ennemy3x = minx + 1; }
+    if (ennemy3y <= miny) { ennemy3y = miny + 1; }
+    if (ennemy3x >= maxx) { ennemy3x = maxx - 1; }
+    if (ennemy3y >= maxy) { ennemy3y = maxy - 1; }
 
     document.getElementById('playerman').style.left = playerx * playerw + "px";
-    document.getElementById('playerman').style.top = playery * playerh + 12.5+"px";
-    document.getElementById('posX').innerHTML = playerx;
+    document.getElementById('playerman').style.top = playery * playerh + playerh / 2 + "px";
+    document.getElementById('ennemy1').style.left = ennemy1x * ennemyw + "px";
+    document.getElementById('ennemy1').style.top = ennemy1y * ennemyh + ennemyh / 2 + "px";
+    document.getElementById('ennemy2').style.left = ennemy2x * ennemyw + "px";
+    document.getElementById('ennemy2').style.top = ennemy2y * ennemyh + ennemyh / 2 + "px";
+    document.getElementById('ennemy3').style.left = ennemy3x * ennemyw + "px";
+    document.getElementById('ennemy3').style.top = ennemy3y * ennemyh + ennemyh / 2 + "px";
+
+    // Pour debugger et comprendre
+    let posXid = document.getElementById('posX');
+    console.log(posXid.style);
+
+    posXid.innerHTML = playerx;
+
+    var elem = document.getElementById("playerman");
+    var theCSSprop = parseInt(window.getComputedStyle(elem, null).getPropertyValue("width"));
+
+    console.log(theCSSprop);
+
     document.getElementById('posY').innerHTML = playery;
-
-    console.log(playerid);
-    console.log(playerh);
-
-    // document.getElementById('texto').innerText = "Textb";
+    // Fin - Pour debugger et comprendre
 
 };
